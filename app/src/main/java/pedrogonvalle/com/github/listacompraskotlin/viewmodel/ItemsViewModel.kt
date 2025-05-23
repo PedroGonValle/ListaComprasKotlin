a3907daa0f5aa77a0792d88df6fd9676c1ba8e1c
pedrogonvalle <pedrogonvalle@gmail.com>
2025-05-22 21:19:03 -0300

    corrigindo documentação

--- app/src/main/java/pedrogonvalle/com/github/listacompraskotlin/viewmodel/ItemsViewModel.kt
+++ app/src/main/java/pedrogonvalle/com/github/listacompraskotlin/viewmodel/ItemsViewModel.kt
@@ -11,15 +11,6 @@ import pedrogonvalle.com.github.listacompraskotlin.data.ItemDao
 import pedrogonvalle.com.github.listacompraskotlin.data.ItemDatabase
 import pedrogonvalle.com.github.listacompraskotlin.model.ItemModel
 
-/**
- * Uma classe ViewModel que estende AndroidViewModel. Esta classe é responsável por gerenciar e preparar os dados para a UI.
- * Ela mantém uma referência para o DAO e um LiveData que contém a lista de itens.
- * A classe também fornece métodos para adicionar e remover itens.
- *
- * @author Ewerton Carreira
- * @version 1.0
- * @since 2023-03-01
- */
 class ItemsViewModel(application: Application) : AndroidViewModel(application) {
 
     private val itemDao: ItemDao
